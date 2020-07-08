50072d08e085df014842902deea9d39fab31d9d4
Tigran <tigran.danielyan3@gmail.com>
2020-07-08 22:18:45 +0400

    This file is about python classes

---
+++ classes.py
@@ -0,0 +1,94 @@
+# # class Resume:
+
+# # 	def __init__(self,name,surname):
+
+# # 		self.applicant_name = name
+# # 		self.applicant_surname = surname
+
+
+
+# # 	company_name = "FreeDOM Data" 
+
+
+
+# # my_resume = Resume()
+# # your_resume = Resume()
+
+# # print(my_resume.company_name)
+# # print(your_resume.company_name)
+
+# # your_resume.company_name = "Digitain"
+
+# # print(your_resume.company_name)
+
+# # print(my_resume.company_name)
+
+# # Resume.company_name = "pghik"
+
+# # print(your_resume.company_name)
+
+# # print(my_resume.company_name)
+
+# # his_resume = Resume()
+# # print(his_resume.company_name)
+
+# class Resume:
+# 	company_name = "FreeDOM Data" 
+
+# 	def __init__(self,name,surname,age=20):
+
+# 		self.applicant_name = name
+# 		self.applicant_surname = surname
+# 		self.applicant_age = age
+
+# 	def applicant_full_name(self):
+# 		self.full_name = F"{self.applicant_name} {self.applicant_surname}"
+		
+# 		return self.full_name
+
+# 	def set_applicant_age(self,age):
+# 		self.applicant_age = age
+
+
+
+
+
+
+
+
+
+# my_resume = Resume("Tigran","Danielyan",24)
+
+# your_resume = Resume("Srbuhi","Chalikyan")
+
+# print(my_resume.__dict__)
+# print(your_resume.__dict__)
+
+# # print(my_resume.applicant_name)
+# # print(my_resume.applicant_surname)
+# # print(my_resume.company_name)
+# # print(my_resume.applicant_age)
+# # print(your_resume.applicant_age)
+
+# # my_resume.applicant_age = 23
+# # print(my_resume.applicant_age)
+# # print(your_resume.applicant_age)
+
+# print(my_resume.applicant_full_name())
+# print(your_resume.applicant_full_name())
+
+# # your_resume.applicant_age = 22
+# your_resume.set_applicant_age(22)
+# print(your_resume.applicant_age)
+
+# ## Create Full  Resume application form  with classes##
+# # it should contain 2 methods one getter one setter
+
+# ## Create class for Cars, wich should contain some information about cars : the information should be given via constuctor 
+# # (mark, year, color)##
+
+# def func(l:j):
+# 	print (l)
+
+# func(14)
+
